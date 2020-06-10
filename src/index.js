75b94630b9a1ac4b7c0e097c572089fda691694b
Aleksey-Shtr1h <52354908+Aleksey-Shtr1h@users.noreply.github.com>
2020-06-11 01:59:14 +0700

    module1-task3

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  PLACES_COUNT: 93,
+};
+
+ReactDOM.render(
+    <App
+      placesCount = {Settings.PLACES_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
