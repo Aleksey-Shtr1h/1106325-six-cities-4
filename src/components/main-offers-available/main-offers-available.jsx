9a0801c236855855cccac0f911ecd03dfe596e9d
Aleksey-Shtr1h <52354908+Aleksey-Shtr1h@users.noreply.github.com>
2020-07-07 18:33:36 +0700

    edit

--- src/components/main-offers-available/main-offers-available.jsx
+++ src/components/main-offers-available/main-offers-available.jsx
@@ -5,6 +5,10 @@ import {MapCities} from '../map-cities/map-cities.jsx';
 
 import {propsTypeAll} from "../../propsType/propsType.js";
 
+const testFoo = (value) => {
+  console.log(value);
+}
+
 export const MainOffersAvailable = ({placesCount, cityActive, offers, onTitlePlaceClick, cityCoordinates}) => {
 
   const cityMap = `city`;
@@ -16,6 +20,8 @@ export const MainOffersAvailable = ({placesCount, cityActive, offers, onTitlePla
         <b className="places__found">{placesCount} places to stay in {cityActive}</b>
         <form className="places__sorting" action="#" method="get">
           <span className="places__sorting-caption">Sort by</span>
+
+          {/*
           <span className="places__sorting-type" tabIndex="0">
             Popular
             <svg className="places__sorting-arrow" width="7" height="4">
@@ -29,14 +35,39 @@ export const MainOffersAvailable = ({placesCount, cityActive, offers, onTitlePla
             <li className="places__option" tabIndex="0">Top rated first</li>
           </ul>
 
-          {/*
-          <select className="places__sorting-type" id="places-sorting">
-            <option className="places__option" value="popular" selected="">Popular</option>
-            <option className="places__option" value="to-high">Price: low to high</option>
-            <option className="places__option" value="to-low">Price: high to low</option>
-            <option className="places__option" value="top-rated">Top rated first</option>
+          <select defaultValue={"B"}>
+            <option>{"A"}</option>
+            <option>{"B"}</option>
+            <option>{"C"}</option>
+
           </select>
+
           */}
+
+          <select
+            className="places__sorting-type"
+            id="places-sorting"
+            value={`popular`}
+            onChange={(evt) => testFoo(evt.target.value)}
+          >
+            <option className="places__option" value="popular">
+              Popular
+            </option>
+
+            <option className="places__option" value="to-high">
+              Price: low to high
+            </option>
+
+            <option className="places__option" value="to-low">
+              Price: high to low
+            </option>
+
+            <option className="places__option" value="top-rated">
+              Top rated first
+            </option>
+
+          </select>
+
 
         </form>
         <div className="cities__places-list places__list tabs__content">
