a3fb911f7f0cccd0d5117c293d24adcfce351eb1
Aleksey-Shtr1h <52354908+Aleksey-Shtr1h@users.noreply.github.com>
2020-08-05 22:05:36 +0700

    05.08.20_22:05

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import {ReviewsItem} from '../reviews-item/reviews-item.jsx';
 import {WrapperUserNewReview} from '../user-new-review/user-new-review.jsx';
 
+import {AuthorizationStatus} from '../../store/user-reducer/user-reducer.js';
+
 import {propsTypeAll} from "../../propsType/propsType.js";
 
-export const ReviewsList = ({reviews}) => {
+export const ReviewsList = ({reviews, authorizationStatus}) => {
   const reviewsCount = reviews.length;
 
   return (
@@ -27,96 +29,9 @@ export const ReviewsList = ({reviews}) => {
         };
       </ul>
 
-      <WrapperUserNewReview />
-
-      {/* <form className="reviews__form form" action="#" method="post">
-        <label className="reviews__label form__label" htmlFor="review">Your review</label>
-        <div className="reviews__rating-form form__rating">
-          {ratingKeys.map((key, id) => {
-
-            const starId = id + 1;
-
-            return (
-              <React.Fragment key={key + id}>
-
-                <input
-                  className="form__rating-input visually-hidden"
-                  name="rating"
-                  value={starId}
-                  id={`${starId}-stars`}
-                  type="radio"
-                />
-
-                <label
-                  htmlFor={`${starId}-stars`}
-                  className="reviews__rating-label form__rating-label"
-                  title={ratingTitle[key]}
-                >
-                  <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                  </svg>
-                </label>
-
-              </React.Fragment>
-            );
-          })
-          .sort((a, b) => b.key - a.key)}
-
-           <input
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value="5"
-            id="5-stars"
-            type="radio"
-          />
-
-          <label
-            htmlFor="5-stars"
-            className="reviews__rating-label form__rating-label"
-            title="perfect"
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-        </div>
-
-        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-        <div className="reviews__button-wrapper">
-          <p className="reviews__help">
-            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-          </p>
-          <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
-        </div>
-      </form> */}
+      {authorizationStatus === AuthorizationStatus.AUTH
+        &&
+      <WrapperUserNewReview/>}
 
     </section>
   );
